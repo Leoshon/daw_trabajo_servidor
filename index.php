4b5d7a60feb502e388a3ea2a853edcbf40472e61
Leoshon <131702139+Leoshon@users.noreply.github.com>
2024-01-05 09:24:21 +0100

    second commit

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['usuario'])) {
       <h1>Tienda Medac Sport</h1>
     </div>
     <nav>
-      <a href="cliente/alta.php">Registrase</a>
+      <a href="cliente/alta.php">Registrarse</a>
     </nav>
   </header>
 
